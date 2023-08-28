bbf9ef76907996c753b90ab5066860afa531b659
Rohan-Navale <rohannavalekannadiga@gmail.com>
2023-08-28 18:59:54 +0530

    Class works perfectly

--- src/RoundedTextField.java
+++ src/RoundedTextField.java
@@ -4,19 +4,27 @@ import java.awt.*;
 
 public class RoundedTextField extends JTextField {
     private int radius;
+    private int borderWidth;
+    private String initialText;
 
-    public RoundedTextField(String placeholder, int columns, int radius, int width) {
+    public RoundedTextField(String placeholder, int columns, int radius, int borderWidth) {
         super(placeholder, columns);
         this.radius = radius;
+        this.borderWidth = borderWidth;
+        this.initialText = placeholder;
+
+        setBackground(Color.WHITE);
+        setCaretColor(Color.BLACK);
         setForeground(Color.GRAY);
         setFont(new Font("Raleway", Font.BOLD, 22));
         setOpaque(false);
-        setBorder(new RoundedBorder(radius, width));
+        setBorder(new RoundedBorder(radius, borderWidth));
 
+        setText(placeholder); // Set initial text
         addFocusListener(new java.awt.event.FocusAdapter() {
             @Override
             public void focusGained(java.awt.event.FocusEvent evt) {
-                if (getText().trim().equals(placeholder.trim())) {
+                if (getText().trim().equals(initialText.trim())) {
                     setText("");
                     setForeground(Color.BLACK);
                 }
@@ -25,7 +33,7 @@ public class RoundedTextField extends JTextField {
             @Override
             public void focusLost(java.awt.event.FocusEvent evt) {
                 if (getText().trim().isEmpty()) {
-                    setText(placeholder);
+                    setText(initialText);
                     setForeground(Color.GRAY);
                 }
             }
@@ -45,7 +53,7 @@ public class RoundedTextField extends JTextField {
         public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
             Graphics2D g2d = (Graphics2D) g;
             g2d.setColor(getForeground());
-            g2d.setStroke(new BasicStroke(borderWidth)); // Set the desired border width
+            g2d.setStroke(new BasicStroke(borderWidth));
             g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
             g2d.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
         }
