5320d13231f304c713704e47a9a23fb7118b3006
Rohan-Navale <rohannavalekannadiga@gmail.com>
2023-08-31 09:09:12 +0530

    Logo added

--- src/Register.java
+++ src/Register.java
@@ -9,6 +9,13 @@ public class Register extends JFrame {
         setLocation(0,0);
         setLayout(null);
 
+        ImageIcon LogoIcon = new ImageIcon(ClassLoader.getSystemResource("Icons/logo.png")); // Load the image
+        Image LogoImage = LogoIcon.getImage();
+        Image LogoscaledImage = LogoImage.getScaledInstance(180, 74, Image.SCALE_SMOOTH);
+        ImageIcon LogoscaledIcon = new ImageIcon(LogoscaledImage);
+        JLabel Logolabel = new JLabel(LogoscaledIcon);
+        Logolabel.setBounds(20, 25, 180, 74); // Set the position and size of the JLabel
+        add(Logolabel);
 
         JPanel panel = new JPanel();
         panel.setBounds(750,0,750,800);
