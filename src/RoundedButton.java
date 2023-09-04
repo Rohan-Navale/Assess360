d83aeddf41457f7a1a3cb315460bdf5cebc4dea3
Rohan-Navale <rohannavalekannadiga@gmail.com>
2023-09-04 09:28:46 +0530

    On hover button color issue fixed.

--- src/RoundedButton.java
+++ src/RoundedButton.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.geom.RoundRectangle2D;
 
 public class RoundedButton extends JButton {
@@ -19,11 +21,26 @@ public class RoundedButton extends JButton {
         setContentAreaFilled(false);
         setFocusPainted(false);
         setForeground(textColor);
-        setFont(new Font("Rale-way", Font.BOLD, 12));
+        setFont(new Font("Raleway", Font.BOLD, 12));
         setBorderPainted(false);
         setHorizontalAlignment(SwingConstants.CENTER); // Center the text horizontally
         setVerticalAlignment(SwingConstants.CENTER);
         setMargin(new Insets(10, 20, 10, 20));
+
+        // Add mouse listener to change colors on hover and click
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                setForeground(Color.WHITE); // Change text color on hover
+                setBackground(bgColor.darker()); // Darken button color on hover
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                setForeground(textColor); // Reset text color on exit
+                setBackground(bgColor); // Reset button color on exit
+            }
+        });
     }
 
     @Override
@@ -34,14 +51,7 @@ public class RoundedButton extends JButton {
         RoundRectangle2D.Float roundRect = new RoundRectangle2D.Float(
                 0, 0, getWidth() - 1, getHeight() - 1, 35, 44);
 
-        if (getModel().isArmed()) {
-            g2.setColor(bgColor.darker());
-        } else if (getModel().isRollover()) {
-            g2.setColor(bgColor.brighter());
-        } else {
-            g2.setColor(bgColor);
-        }
-
+        g2.setColor(getBackground());
         g2.fill(roundRect);
 
         super.paintComponent(g2);
