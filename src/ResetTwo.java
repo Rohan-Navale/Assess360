e1f3e836bfa0915cc8f07e6483dbbbe8e28a6f4e
Rohan-Navale <rohannavalekannadiga@gmail.com>
2023-09-05 07:07:26 +0530

    Textfields added

--- src/ResetTwo.java
+++ src/ResetTwo.java
@@ -1,3 +1,5 @@
+import com.toedter.calendar.JDateChooser;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -17,7 +19,29 @@ public class ResetTwo  extends JFrame{
         Logolabel.setBounds(20, 25, 180, 74); // Set the position and size of the JLabel
         add(Logolabel);
 
+        ImageIcon originalIcon = new ImageIcon(ClassLoader.getSystemResource("Icons/reset.png")); // Load the image
+        Image originalImage = originalIcon.getImage();
+        Image scaledImage = originalImage.getScaledInstance(609, 624, Image.SCALE_SMOOTH);
+        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+        JLabel label = new JLabel(scaledIcon);
+        label.setBounds(62, 130, 609, 624); // Set the position and size of the JLabel
+        add(label);
 
+        JLabel text  = new JLabel("Enter new your password");
+        text.setFont(new Font("Raleway",Font.BOLD, 30));
+        text.setBounds(830,164,700,40);
+        text.setForeground(Color.BLACK);
+        add(text);
+
+        RoundedTextField newpass = new RoundedTextField(20, 15, 10,"New Password");
+        newpass.setBounds(822, 230, 450, 55);
+        newpass.setBackground(Color.WHITE);
+        add(newpass);
+
+        RoundedTextField repass = new RoundedTextField(20, 15, 10,"Re-enter New Password");
+        repass.setBounds(822, 308, 450, 55);
+        repass.setBackground(Color.WHITE);
+        add(repass);
 
     }
 
