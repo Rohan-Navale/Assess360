f9801d9da8d398d4437a13acfbd4e7e353cdf3a8
Rohan-Navale <rohannavalekannadiga@gmail.com>
2023-09-06 13:45:48 +0530

    Database created "onlineexams"

--- src/DbConnectivity.java
+++ src/DbConnectivity.java
@@ -1,3 +1,13 @@
+import java.sql.*;
 public class DbConnectivity {
-
+    Connection  c;
+    Statement s;
+    public DbConnectivity() {
+        try {
+            c = DriverManager.getConnection("jdbc:mysql:///onlineexams", "root", "123456789");
+            s = c.createStatement();
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
 }
