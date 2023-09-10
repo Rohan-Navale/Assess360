5ec65bc08891fbbe463af9510633807dba529923
Rohan-Navale <rohannavalekannadiga@gmail.com>
2023-09-10 16:10:19 +0530

    Methods added to verify password reset credentials

--- src/DbConnectivity.java
+++ src/DbConnectivity.java
@@ -1,6 +1,9 @@
 import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class DbConnectivity {
     Connection  c;
     Statement s;
@@ -12,4 +15,40 @@ public class DbConnectivity {
             System.out.println(e);
         }
     }
+
+    public PreparedStatement prepareStatement(String sql) throws SQLException {
+        return c.prepareStatement(sql);
+    }
+
+    public boolean usnExists(String usn) {
+        try {
+            String sql = "SELECT * FROM register WHERE USN=?";
+            PreparedStatement preparedStatement = c.prepareStatement(sql);
+            preparedStatement.setString(1, usn);
+            ResultSet rs = preparedStatement.executeQuery();
+            return rs.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+    public boolean dateOfBirthMatches(String usn, String dateOfBirth) {
+        try {
+            String sql = "SELECT COUNT(*) FROM register WHERE USN = ? AND DOB = ?";
+            PreparedStatement preparedStatement = prepareStatement(sql);
+            preparedStatement.setString(1, usn);
+            preparedStatement.setString(2, dateOfBirth);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next() && resultSet.getInt(1) > 0) {
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
 }
