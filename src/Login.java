8bf2fed52a59582091da3a52cbba328b7f1ac012
Rohan-Navale <rohannavalekannadiga@gmail.com>
2023-09-21 21:38:24 +0530

    update

--- src/Login.java
+++ src/Login.java
@@ -2,9 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 public class Login extends JFrame implements ActionListener {
-
     RoundedTextField usnField, PasswordField;
     JButton Login, Register, forgot;
     Login(){
@@ -124,6 +122,5 @@ public class Login extends JFrame implements ActionListener {
                 }
             }
         }
-
     }
 }
